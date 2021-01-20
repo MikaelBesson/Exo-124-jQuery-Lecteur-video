6f0a714b592ce8c16afba7b2831f8aba12c177af
Besson Mikael <73292500+MikaelBesson@users.noreply.github.com>
2021-01-20 10:17:20 +0100

    lecteur video jQuery

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 // FIXME Assurez vous que ces deux variables contiennent bien les bonnes choses ;-)
-let player = $('.player__video').first();
+let player = $('.player__button').first();
+let media = $(".player__video")[0];
 let progress = $('.progress__filled').first();
+
+player.click(playPauseMedia);
+
+function playPauseMedia() {
+    if (media.paused) {
+        player.html("&#10074;");
+        media.play();
+    } else {
+        player.html("&#9658;");
+        media.pause();
+    }
+}
+
+//pause ==> &#10074;
+//play ==> &#9658;
